9b15d02747e333a9340c04691864f2e272322eb1
yhm (amber) <103625580+yhm-amber@users.noreply.github.com>
2022-05-08 13:22:27 +0800

    Create node-tools.sh

---
+++ tools/node-tools.sh
@@ -0,0 +1,29 @@
+pack_servicelogs ()
+{
+    : usage demo: pack_servicelogs kubelet 2021-10-11 2021-12-22 /mnt/logs ;
+    
+    local service_name="${1}" &&
+    local date_bg="${2}" &&
+    local date_ed="${3}" &&
+    local save_place="${4:-.}" &&
+    
+    local outname="$(echo $service_name | tr / :)"-logs-"$HOSTNAME"-"$date_bg"_"$date_ed" &&
+    
+    get_files ()
+    {
+        : usage demo: get_files inceptor1 2021-10-11 2021-12-22 ;
+        
+        find -- /var/log/"${1:-$service_name}" \
+          -name '*.log*' \
+          -newermt "${2:-$date_bg}" \
+          -and ! -newermt "${3:-$date_ed}" &&
+        
+        :;
+    } &&
+    
+    tar -cf- -- $(get_files "$service_name" "$date_bg" "$date_ed") |
+        xz -T0 --best > "$save_place"/"$outname".tar.xz &&
+    
+    ls "$save_place"/"$outname".tar.xz &&
+    :;
+} ;
