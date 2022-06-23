2bbee68e3bb07fa8bf66d373b819b56c3039f1b0
yhm (amber) <103625580+yhm-amber@users.noreply.github.com>
2022-06-23 16:27:48 +0800

    Update src.sh

--- build-practices/targeto/src.sh
+++ build-practices/targeto/src.sh
@@ -64,10 +64,7 @@ targeto ()
         
         dockerfile_echos ()
         {
-            echo  FROM alpine
-            
-            echo  RUN sed -i '"s/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g"' /etc/apk/repositories
-            echo  RUN apk --no-cache add -- bash tar zstd
+            echo  FROM targeto
             
             echo  WORKDIR /"$IMG_NAME"
             
