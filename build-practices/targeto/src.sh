7acdceb1daf8e23e5434f2da13bb39f848de23ee
yhm (amber) <103625580+yhm-amber@users.noreply.github.com>
2022-06-26 17:35:58 +0800

    Update src.sh

--- build-practices/targeto/src.sh
+++ build-practices/targeto/src.sh
@@ -9,12 +9,18 @@
 targeto ()
 {
     : demo
-    : targeto gettor pkg/disc img-name /opt/sdk:/opt/sdk $PWD/app:/opt/app
+    
+    : targeto gettor img-name /opt/sdk:/opt/sdk $PWD/app:/opt/app
+    : targeto gettor _disc /opt/sdk:sdk:/opt/sdk $PWD/app:app:/opt/app
     
     : should
-    : get a oci img include tar.xz/zst
+    
+    : get a oci img include tar.zst
+    : get some oci imgs include their tar.zst
     
     : and
+    
+    : descs
     : descs
         
     :;
@@ -24,28 +30,106 @@ targeto ()
     {
         :;
         
-        
-        local tgt_type="$1" && shift 1 &&
         local image_name="$1" && shift 1 &&
         
+        mode__disc ()
+        {
+            : make
+            
+            : mode__disc /opt/sdk:sdk:/opt/sdk $PWD/app:app:/opt/app
+            
+            : as
+            
+            : gettor sdk /opt/sdk:.
+            : gettor app $PWD/app:.
+            
+            :;
+            
+            disc_per ()
+            {
+                : disc_per $PWD/app app /opt/app
+                
+                :;
+                
+                local disc_dir="$1" && shift 1 &&
+                local disc_name="$1" && shift 1 &&
+                local disc_for="$1" && shift 1 &&
+                
+                gettor disc-"$disc_name" "$disc_dir":.
+                
+                :;
+                
+            } &&
+            
+            disc_per_o ()
+            {
+                : disc_o $PWD/app app /opt/app
+                
+                :;
+                
+                local disc_dir="$1" && shift 1 &&
+                local disc_name="$1" && shift 1 &&
+                local disc_for="$1" && shift 1 &&
+                
+                echo disc: now you may want your "'$disc_dir'" in disc-"$disc_name" mount as "'$disc_for'" in some container.
+                
+                :;
+                
+            } &&
+            
+            
+            export -f -- disc_per disc_per_o || { echo fun err ; return 7 ; }
+            
+            :;
+            
+            
+            rtb "$@" | xargs -i -- $SHELL -c 'disc_per {}' &&
+            
+            echo :::::::: :: :::::::: :::::::: :: :::::::: &&
+            
+            rtb "$@" | xargs -i -- $SHELL -c 'disc_per_o {}' &&
+            
+            :;
+            
+        } &&
         
-        (
+        
+        if type -f -- mode_"$image_name" ;
+        
+        then
+            
+            : special mode way
+            
+            (mode_"$image_name" "$@") &&
+            
+            :;
+            
+        else
+            
+            : normal mode way
+            
             mkdir -p -- "$image_name" ;
             
-            cd "$image_name" &&
+            (
+                cd "$image_name" &&
+                
+                
+                d "$image_name" .tgt "$@" > Dockerfile &&
+                s targeto > src.sh &&
+                
+                c $(F=1 rtb "$@") > "$image_name".tar.zst &&
+                
+                eval "$(p "$image_name")" &&
+                
+                : ) &&
             
+            o "$image_name" .tgt &&
             
-            d "$image_name" "$tgt_type" "$@" > Dockerfile &&
-            s targeto > src.sh &&
+            :;
             
-            c $(F=1 rtb "$@") > "$image_name".tar.zst &&
-            
-            eval "$(p "$image_name")" &&
-            
-            
-            : ) &&
+        fi &&
         
-        o "$image_name" "$tgt_type" &&
+        
         
         :;
         
@@ -54,17 +138,17 @@ targeto ()
     
     d ()
     {
-        : d img-name tgt/disc /opt/sdk:/opt/sdk $PWD/app:/opt/app
+        : d img-name tgtdir-name /opt/sdk:/opt/sdk $PWD/app:/opt/app
         
         :;
         
         local image_name="$1" && shift 1 &&
-        local tgt_type="$1" && shift 1 &&
+        local tgtdir_name="$1" && shift 1 &&
         
         
         
         
-        dockerfile_echos_pkg ()
+        dockerfile_echos ()
         {
             echo  FROM targeto
             
@@ -74,40 +158,21 @@ targeto ()
             echo  COPY "$IMG_NAME".tar.zst ./
             
             echo  ENV DIR_PAIRS="'$DIR_PAIRS'"
-            echo  RUN mkdir .pkg '&&' cd .pkg '&&' mkdir -p -- $DIR_AIMS
+            echo  RUN mkdir "$DIR_AIM" '&&' cd "$DIR_AIM" '&&' mkdir -p -- $DIR_AIMS
             
             echo  ENTRYPOINT '["bash","src.sh"]'
-            echo  CMD '["targeto","x","pkg","'"$IMG_NAME"'.tar.zst"]'
-            
-            :;
-            
-        } &&
-        
-        dockerfile_echos_disc ()
-        {
-            echo  FROM targeto
-            
-            echo  WORKDIR /"$IMG_NAME"
-            
-            echo  COPY Dockerfile src.sh ./
-            echo  COPY "$IMG_NAME".tar.zst ./
-            
-            echo  ENV DIR_PAIRS="'$DIR_PAIRS'"
-            echo  RUN mkdir .disc
-            
-            echo  ENTRYPOINT '["bash","src.sh"]'
-            echo  CMD '["targeto","x","disc","'"$IMG_NAME"'.tar.zst"]'
+            echo  CMD '["targeto","x","'"$IMG_NAME"'.tar.zst","'"$DIR_AIM"'"]'
             
             :;
             
         } &&
         
         
-        IMG_NAME="$image_name" DIR_PAIRS="$*" DIR_AIMS=$(
+        IMG_NAME="$image_name" DIR_AIM="$tgtdir_name" DIR_AIMS=$(
             
             F='(NF+1)"./\""$2"\""' rtb "$@" &&
             
-            : ) dockerfile_echos_"$tgt_type" &&
+            : ) DIR_PAIRS="$*" dockerfile_echos &&
         
         :;
         
@@ -130,7 +195,7 @@ targeto ()
         
     } &&
     
-    c () # need type.
+    c ()
     {
         : c /opt/sdk xxx/app
         
@@ -161,31 +226,31 @@ targeto ()
     
     o ()
     {
-        : o img-name pkg/disc
+        : o img-name tgtdir-name
         
         :;
         
         local image_name="$1" && shift 1 &&
-        local tgt_type="$1" && shift 1 &&
+        local tgtdir_name="$1" && shift 1 &&
         
         
         echo ; echo  :::::::: :: :::::::: :: :::::::: :: ::::::::
         echo  img: "'$image_name'"
-        echo  usage: docker run --rm -v volume-"'$image_name'":"'/${image_name}/.${tgt_type}'" -- "'$image_name'"
-        echo  then: You have your things in your volume now 🦾！ ; echo
+        echo  usage: docker run --rm -v volume-"'$image_name'":"'/${image_name}/${tgtdir_name}'" -- "'$image_name'"
+        echo  then: You have your aim things in your volume now 🦾！ ; echo
     } &&
     
     x ()
     {
-        : x pkg/disc img-name.tar.zst
+        : x img-name.tar.zst target-dir
         
         : xz: -c, --stdout, --to-stdout
         : xz: -d, --decompress, --uncompress
         
         :;
         
-        local tgt_type="$1" && shift 1 &&
         local xzfile="$1" && shift 1 &&
+        local tgtdir="$1" && shift 1 &&
         
         (
             mkdir -p -- from ;
@@ -201,7 +266,7 @@ targeto ()
         
         eval "$(
             
-            F='(NF+1)"mv","from/\""$1"\"","'".$tgt_type"'/\""$2"\""' rtb $DIR_PAIRS &&
+            F='(NF+1)"mv","from/\""$1"\"","'"$tgtdir"'/\""$2"\""' rtb $DIR_PAIRS &&
             
             : )" &&
         
