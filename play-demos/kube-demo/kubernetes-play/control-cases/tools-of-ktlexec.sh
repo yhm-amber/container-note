e3b7c12c0b4e5e58effd202d7c22ad61db9e755a
yhm (amber) <103625580+yhm-amber@users.noreply.github.com>
2022-12-25 15:07:04 +0800

    Update and rename kubectl-tools.sh to tools-of-ktlexec.sh

--- play-demos/kube-demo/kubernetes-play/control-cases/tools-of-ktlexec.sh
+++ play-demos/kube-demo/kubernetes-play/control-cases/tools-of-ktlexec.sh
@@ -2,41 +2,6 @@
 
 
 
-klsctl ()
-{
-    : usage demo:
-    : klsctl nodexx/NotReady status kubelet
-    
-    : ::::::::::: :
-    
-    (($# != 3)) && echo para err 🥲 && return 233 ;
-    
-    local namefix="${1}" &&
-    local cmd="${2}" &&
-    local svcs="${3}" &&
-    
-    kubectl get no |
-        
-        awk /"$namefix"/'{print$1}' |
-        
-        xargs -i -- ssh {} -- '
-            
-            echo : :::: $HOSTNAME ... start ... $(date +%s) :::: : ;
-            
-            systemctl '"'$cmd'"' -- '"$svcs"' ;
-            
-            echo : :::: $HOSTNAME ... done ... $(date +%s) :::: : ' |
-        
-        tee "($svcs)"-"$namefix"-"$cmd".log &&
-    
-    
-    (
-        echo : =========== : ;
-        ls "($svcs)"-"$namefix"-"$cmd".log ) &&
-    
-    :;
-} ;
-
 
 
 
