268f84fddec4bd69d7d232ff7dcbadc27510b278
yhm (amber) <103625580+yhm-amber@users.noreply.github.com>
2023-01-26 10:51:38 +0800

    +

---
+++ practices-notes/Catalytic.rs
@@ -0,0 +1,14 @@
+
+[repo]: https://github.com/Jasperav/Catalytic.git
+[intro-rustcc]: https://rustcc.cn/article?id=803e930a-1172-4da0-b41e-583c63679097
+
+[ScyllaDB B Cassandra 的 ORM | Rust 中文社区][intro-rustcc] : 
+
+> Catalytic 是 ScyllaDb 和 Cassandra 的 ORM ，
+> 它为基于数据库查询和生成 Rust structs 提供了
+> 零成本抽象。 Scylla 和 Cassandra 都是 NoSQL 数据库，
+> 可以水平扩展并保存大量数据。
+> 
+
+repo: [Jasperav/Catalytic | GitHub][repo]
+
