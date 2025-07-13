1f8079b6722d9eab282fdae21e9c64956f95eb54
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-07-13 18:49:05 +0800

    Update tidysave.R

--- play-demos/spyder.web-demo/.scriptdemo/bqgnovels-dgyx/tidysave.R
+++ play-demos/spyder.web-demo/.scriptdemo/bqgnovels-dgyx/tidysave.R
@@ -9,12 +9,6 @@ nvl_50416[, path := .SD |> glue::glue_data("{seqq} {title}/readme")]
 nvl_50416[, file_content := paste0(title, '\n\n~~~~~~~~~\n\n', content, '\n\n')]
 
 'E:/repos/nv50416' |> magrittr::'%T>%'(fs::dir_create()) |> withr::with_dir({
-	# nvl_50416[, .rec := {
-	# 	fs::path_dir(path) |> fs::dir_create()
-	# 	{file_content |> readr::write_file(path)} |> tryCatch(error = base::identity) -> x
-	# 	x |> dplyr::coalesce(NA)
-	# }]
-
 	seq(nrow(nvl_50416)) |> purrr::map(~ {
 		nvl_50416[.x, file_content] |> readr::write_file(
 			nvl_50416[.x, path] |> magrittr::'%T>%'({fs::dir_create(fs::path_dir(.))}))
