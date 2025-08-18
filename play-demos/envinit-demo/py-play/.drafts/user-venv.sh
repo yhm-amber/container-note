a15b58cbd2661acac034234e665ba2942509ad81
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-08-18 19:13:52 +0800

    Update user-venv.sh

--- play-demos/envinit-demo/py-play/.drafts/user-venv.sh
+++ play-demos/envinit-demo/py-play/.drafts/user-venv.sh
@@ -4,8 +4,8 @@ alias pip='python -m pip'
 alias ipykernel='python -m ipykernel'
 
 # basic in jupyter
-pip install -U -- pip wheel polars dfply pipe nest-asyncio rustynum
-pip install -U -- ipykernel ipywidgets shinywidgets jupyter-ydoc jupyterlab-js jupyter-server-proxy jupyter_copilot cognite-jupyterlab-copilot jupyterlab-link-share jupyter-collaboration jupyverse[auth,notebook,jupyterlab] jupyterlab_widgets pwb_jupyterlab jupyterlab_git
+pip install -U -- pip wheel polars dfply pipe rustynum
+pip install -U -- ipykernel ipywidgets nest-asyncio shinywidgets jupyter-ydoc jupyterlab-js jupyter-server-proxy jupyter_copilot cognite-jupyterlab-copilot jupyterlab-link-share jupyter-collaboration jupyverse[auth,notebook,jupyterlab] jupyterlab_widgets pwb_jupyterlab jupyterlab_git
 ipykernel install --user --name "$(whoami)" --display-name "Python (venv user '$(whoami)')"
 jupyter labextension list
 jupyter server extension list
