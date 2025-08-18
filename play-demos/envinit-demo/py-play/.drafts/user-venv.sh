eb7a3cf3560a84b7d7227e1ad26306789f25cf32
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-08-18 18:43:33 +0800

    Create user-venv.sh

---
+++ play-demos/envinit-demo/py-play/.drafts/user-venv.sh
@@ -0,0 +1,14 @@
+# venv by uv
+cd && uv venv --allow-existing --no-project && . .venv/bin/activate && python -m ensurepip --upgrade
+alias pip='python -m pip'
+alias ipykernel='python -m ipykernel'
+
+# basic in jupyter
+pip install -U -- pip wheel polars dfply pipe rustynum
+pip install -U -- ipywidgets ipykernel jupyter-ydoc jupyterlab-js jupyter-server-proxy jupyter_copilot cognite-jupyterlab-copilot jupyterlab-link-share jupyter-collaboration jupyverse[auth,notebook,jupyterlab] jupyterlab_widgets pwb_jupyterlab jupyterlab_git
+ipykernel install --user --name "$(whoami)" --display-name "Python (venv user '$(whoami)')"
+jupyter labextension list
+jupyter server extension list
+
+# axon like torch
+pip install -U -- torch pytorch-lightning
